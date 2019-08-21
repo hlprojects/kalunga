205b713813f2e10fc6fd6d1f7d362652368c77f4
adrianolupossa <adrianolupossa@gmail.com>
2019-08-22 00:56:51 +0100

    add interactivity in the navbar links

--- navBar.php
+++ navBar.php
@@ -1,31 +1,29 @@
-	<div class="navbar-fixed">
+<div class="navbar-fixed">
 	<ul id="dropdown1" class="dropdown-content">
-	  <li><a href="#!">Alterar</a></li>
-	  <li><a href="src/views/signOut.php">Terminar</a></li>
+		<li><a href="#!">Alterar</a></li>
+		<li><a href="src/views/signOut.php">Terminar</a></li>
 	</ul>
-	  <nav class="z-depth-1 " style="background-color: #0e0e0e !important">
-	    <div class="nav-wrapper">
-	      <a href="#" class="brand-logo center"><strong>Kalunga</strong></a>
+	<nav class="z-depth-1 " style="background-color: #0e0e0e !important">
+		<div class="nav-wrapper">
+			<a href="#" class="brand-logo center"><strong>Kalunga</strong></a>
 
-	      <ul id="nav-mobile" class="right hide-on-med-and-down">
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
 
-			<?php 
+				<?php 
 
-				include 'auto.php';		
+					include 'auto.php';		
+				
+					if (isset($_SESSION['idUserPersona'])) {
+						echo '<li><a class="dropdown-trigger" href="#!" data-target="dropdown1">'.$_SESSION['nome'].'<i class="material-icons right">arrow_drop_down</i></a></li>';
+					}else{
+						echo '<li><a href="src/views/signIn.php">Entrar</a></li>
+								<li><a href="src/views/signUp.php">Criar conta</a></li>
+							';
+					}
+							
+				?>  
 
-
-			
-			    if (isset($_SESSION['idUserPersona'])) {
-			    	echo '<li><a class="dropdown-trigger" href="#!" data-target="dropdown1">'.$_SESSION['nome'].'<i class="material-icons right">arrow_drop_down</i></a></li>';
-			    }else{
-			    	echo '<li><a href="src/views/signIn.php">Entrar</a></li>
-				          <li><a href="src/views/signUp.php">Criar conta</a></li>
-			  			';
-			    }
-			         
-			 ?>  
-
-	      </ul>
-	    </div>
-	  </nav>		
-	</div>
+			</ul>
+		</div>
+	</nav>		
+</div>
