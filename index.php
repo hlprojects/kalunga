f7ecc911da19e2df29a7a67e852e45a5b5fdc40d
adrianolupossa <adrianolupossa@gmail.com>
2019-08-22 00:42:10 +0100

    remove floating button

--- index.php
+++ index.php
@@ -276,20 +276,7 @@
 
 
 				</section>
-			</main>        
- 
-
-	<div class="fixed-action-btn">
-	  <a class="btn-floating btn-large black">
-	    <i class="large material-icons">add</i>
-	  </a>
-	  <ul>
-	    <li><a class="btn-floating red"><i class="material-icons">card_giftcard</i></a></li>
-	    <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
-	    <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
-	    <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
-	  </ul>
-	</div>
+			</main>  
 
         <footer class="page-footer" style="background-color: #0e0e0e !important">
           <div class="container">
